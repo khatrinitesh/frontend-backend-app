7b3cd3bf3ff5b0156a845644c62bf8ac1f409daa
nitesh <nitesh.khatri88@gmail.com>
2024-07-26 18:42:31 +0530

    updated

---
+++ frontend-backend-3/client/src/App.tsx
@@ -0,0 +1,18 @@
+// main app component
+import React from 'react'
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Vite React App</h1>
+      </header>
+    </div>
+    </>
+  )
+}
+
+export default App
